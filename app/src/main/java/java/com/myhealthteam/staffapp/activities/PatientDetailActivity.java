a573265d27293bf31ce07335c2b7b8e5f29f0063
iamsadiashakil <iamsadiashakil@gmail.com>
2025-05-17 15:19:03 +0500

    Added Patient View v2

--- app/src/main/java/java/com/myhealthteam/staffapp/activities/PatientDetailActivity.java
+++ app/src/main/java/java/com/myhealthteam/staffapp/activities/PatientDetailActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,10 +20,10 @@ import java.util.List;
 
 public class PatientDetailActivity extends Activity {
 
-    private RecyclerView recyclerBloodPressure, recyclerBodyTemperature, recyclerAllergies;
-    private VitalsHistoryAdapter bloodPressureAdapter, bodyTemperatureAdapter;
+    private RecyclerView recyclerBloodPressure, recyclerBodyTemperature, recyclerPulseRate, recyclerBloodOxygen, recyclerSugarLevel, recyclerAllergies;
+    private VitalsHistoryAdapter bloodPressureAdapter, bodyTemperatureAdapter, pulseRateAdapter, bloodOxygenAdapter, sugarLevelAdapter;
     private AllergiesHistoryAdapter allergiesAdapter;
-    private List<Vital> bloodPressureHistory, bodyTemperatureHistory;
+    private List<Vital> bloodPressureHistory, bodyTemperatureHistory, pulseRateHistory, bloodOxygenHistory, sugarLevelHistory;
     private List<Allergy> allergiesHistory;
 
     @Override
@@ -49,6 +50,9 @@ public class PatientDetailActivity extends Activity {
         // Initialize RecyclerViews
         recyclerBloodPressure = findViewById(R.id.recycler_blood_pressure);
         recyclerBodyTemperature = findViewById(R.id.recycler_body_temperature);
+        recyclerPulseRate = findViewById(R.id.recycler_pulse_rate);
+        recyclerBloodOxygen = findViewById(R.id.recycler_blood_oxygen);
+        recyclerSugarLevel = findViewById(R.id.recycler_sugar_level);
         recyclerAllergies = findViewById(R.id.recycler_allergies);
 
         setupRecyclerViews();
@@ -56,6 +60,9 @@ public class PatientDetailActivity extends Activity {
         // Populate mock data
         populateBloodPressureHistory();
         populateBodyTemperatureHistory();
+        populatePulseRateHistory();
+        populateBloodOxygenHistory();
+        populateSugarLevelHistory();
         populateAllergiesHistory();
     }
 
@@ -71,6 +78,24 @@ public class PatientDetailActivity extends Activity {
         bodyTemperatureAdapter = new VitalsHistoryAdapter(this, bodyTemperatureHistory);
         recyclerBodyTemperature.setLayoutManager(new LinearLayoutManager(this));
         recyclerBodyTemperature.setAdapter(bodyTemperatureAdapter);
+
+        // Pulse Rate RecyclerView
+        pulseRateHistory = new ArrayList<>();
+        pulseRateAdapter = new VitalsHistoryAdapter(this, pulseRateHistory);
+        recyclerPulseRate.setLayoutManager(new LinearLayoutManager(this));
+        recyclerPulseRate.setAdapter(pulseRateAdapter);
+
+        // Blood Oxygen RecyclerView
+        bloodOxygenHistory = new ArrayList<>();
+        bloodOxygenAdapter = new VitalsHistoryAdapter(this, bloodOxygenHistory);
+        recyclerBloodOxygen.setLayoutManager(new LinearLayoutManager(this));
+        recyclerBloodOxygen.setAdapter(bloodOxygenAdapter);
+
+        // Sugar Level RecyclerView
+        sugarLevelHistory = new ArrayList<>();
+        sugarLevelAdapter = new VitalsHistoryAdapter(this, sugarLevelHistory);
+        recyclerSugarLevel.setLayoutManager(new LinearLayoutManager(this));
+        recyclerSugarLevel.setAdapter(sugarLevelAdapter);
 
         // Allergies RecyclerView
         allergiesHistory = new ArrayList<>();
@@ -89,6 +114,24 @@ public class PatientDetailActivity extends Activity {
         bodyTemperatureHistory.add(new Vital("98.6°F", "2025-05-16 08:00 AM", "Normal"));
         bodyTemperatureHistory.add(new Vital("99.5°F", "2025-05-15 08:00 AM", "Fever"));
         bodyTemperatureAdapter.notifyDataSetChanged();
+    }
+
+    private void populatePulseRateHistory() {
+        pulseRateHistory.add(new Vital("72 bpm", "2025-05-16 08:00 AM", "Normal"));
+        pulseRateHistory.add(new Vital("80 bpm", "2025-05-15 08:00 AM", "Normal"));
+        pulseRateAdapter.notifyDataSetChanged();
+    }
+
+    private void populateBloodOxygenHistory() {
+        bloodOxygenHistory.add(new Vital("97%", "2025-05-16 08:00 AM", "Normal"));
+        bloodOxygenHistory.add(new Vital("95%", "2025-05-15 08:00 AM", "Low"));
+        bloodOxygenAdapter.notifyDataSetChanged();
+    }
+
+    private void populateSugarLevelHistory() {
+        sugarLevelHistory.add(new Vital("110 mg/dL", "2025-05-16 08:00 AM", "Normal"));
+        sugarLevelHistory.add(new Vital("140 mg/dL", "2025-05-15 08:00 AM", "Elevated"));
+        sugarLevelAdapter.notifyDataSetChanged();
     }
 
     private void populateAllergiesHistory() {
